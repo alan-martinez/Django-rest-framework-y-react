7b33974311d195014fdb20d0a66ec972ffe0ae24
alan-martinez <alan.martinez5691@alumnos.udg.mx>
2022-08-02 14:53:10 -0500

    Views

--- apps/blog/urls.py
+++ apps/blog/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    
+    path('', BlogListView.as_view()),
+    # path('category/<category_id>', BlogListCategoryView.as_view()),
+    path('<post_slug>', PostDetailView.as_view()),
 ]
--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from apps.category.models import Category
 
-# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework import permissions
+
+from .models import Post
+from .serializers import PostSerializer
+from .pagination import SmallSetPagination, MediumSetPagination, LargeSetPagination
+
+class BlogListView(APIView):
+    def get(self, request, format=None):
+        if Post.postobjects.all().exists():
+            post = Post.postobjects.all()
+            
+            paginator = SmallSetPagination()
+            results = paginator.paginate_queryset(post, request)
+            serializer = PostSerializer(results, many = True)
+            
+            return paginator.get_paginated_response({'posts': serializer.data})
+        else:
+            return Response({'Error': 'no post found'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        
+        
+class PostDetailView(APIView):
+    def get(self, request, post_slug,format=None):
+        post = get_object_or_404(Post, slug=post_slug)
+        serializer = PostSerializer(post)
+        return Response({'post':serializer.data}, status=status.HTTP_200_OK)
+            
